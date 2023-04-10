400d9d2e8616e16f5ecc86401c8dddd813cc512c
Alan.Chu <achu61@seas.upenn.edu>
2023-04-10 13:21:14 -0700

    added validate new score

--- ratings.py
+++ ratings.py
@@ -6,22 +6,37 @@ def restaurant_ratings(filename):
     #key: restaurant
     #value: ratings
     for line in open(filename):
-        restaurant, ratings = line.rstrip().split(":")
-        scores[restaurant] = ratings
+        restaurant, rating = line.rstrip().split(":")
+        scores[restaurant] = rating
         # print(f"{restaurant} is rated at {ratings}.")
 
 
-def add_ratings():
+def add_rating():
     restaurant = input("Please enter the restaurant name? ")
-    ratings = input("Please enter the rating? ")
-    scores[restaurant] = ratings
+    rating = False
+    while rating == False:
+        rating = validate_rating(input("Please enter the rating? "))
+        print(rating)
+
+    scores[restaurant] = rating
 
 
 def show_ratings():
-    for restaurant, ratings in sorted(scores.items()):
-        print(f"{restaurant} is rated at {ratings}.")
+    for restaurant, rating in sorted(scores.items()):
+        print(f"{restaurant} is rated at {rating}.")
 
 
-add_ratings()
+def validate_rating(rating):
+    try:
+        rating = int(rating)
+        if rating >= 1 and rating <= 5:
+            return rating
+        else:
+            return False
+    except:
+        return False
+
+
+add_rating()
 restaurant_ratings('scores.txt')
 show_ratings()
